ec82540faad7413925e106f557d93672e40ddc92
Oleg Karpenko <olkieah1@gmail.com>
2025-03-02 17:01:46 +0100

    Improved records displaying with tables

--- source/scores.py
+++ source/scores.py
@@ -1,31 +1,38 @@
 import os
 import json
+from prettytable import PrettyTable
 
 class ScoreRecord():
     
     def __init__(self):
-        self.scores = {}
+        self._scores = {}
 
     def __str__(self):
-        res = ""
-        for player in self.scores:
-            res += (f"{player} score: {self.scores[player]}\n")
-        return res
+        res = PrettyTable()
+        res.field_names = ["Player", "Best score"]
+        for player in self._scores:
+            res.add_row([f"{player}", f"{self._scores[player]}"])
+        return res.get_string(sortby="Best score", reversesort=True)
+    
+    def getScores(self):
+        return self._scores
     
     def addPlayer(self, name:str):
-        assert name not in self.scores, f"Player {name} is already at a Score board!"
-        self.scores[name] = 0
+        assert name not in self._scores, f"Player {name} is already at a Score board!"
+        self._scores[name] = 0
 
     def changeScore(self, player:str, score:int):
-        self.scores[player] = score
+        if player not in self._scores:
+            print(f"A new player {player} is being created!")
+        self._scores[player] = score
 
     def importScoreBoard(self, file:str="records.json"):
         assert os.path.isfile(file), f"The file \"{file} \" wasn't found or doesn't exist!"
         with open(file, "r") as scoreBoard:
-            self.scores = json.loads(scoreBoard.read())
+            self._scores = json.loads(scoreBoard.read())
 
     def exportScoreBoard(self, file:str="records.json"):
         if not os.path.isfile(file):
             print(f"A new file with name {file} is being created!")
         with open(file, "w") as scoreBoard:
-            scoreBoard.write(json.dumps(self.scores))
+            scoreBoard.write(json.dumps(self._scores))
