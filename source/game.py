eabc03690a078702a3ed7d3826c79f57155061db
Oleg Karpenko <olkieah1@gmail.com>
2025-03-04 09:11:12 +0100

    Added average score and exeptions for zero

--- source/game.py
+++ source/game.py
@@ -31,37 +31,43 @@ class Game():
             [question, answer] = self._qgen.genRandom()
             print(f"What is {question} ?")
             userAns = input("Your answer: ")
+            while not userAns.replace('-', '').isnumeric():
+                print(f"\n{self._player}, your number should be an integer!\n")
+                userAns = input("Your answer: ")
             if int(userAns) != answer:
                 self._gameOver = True
-                print(f"Holy Linus! The answer you gave is preposterously wrong!\n The correct answer is {answer}")
+                print(f"\nHoly Linus! The answer you gave is preposterously wrong!\n The correct answer is {answer}\n")
     
     @_timer
     def _quiz(self):
-        cnt = 0
+        cnt = 1
         while not self._gameOver:
             print(f"\nQuestion number {cnt}:")
             cnt += 1
             time = self._question()
             if time > 10:
                 self._gameOver = True
-                print(f"Dear {self._player}, I hate to tell you, but at the moment your brain operates like windows vista - ridiculously slow!")
-        self._current = cnt
-    
+                print(f"\nDear {self._player}, I hate to tell you, but at the moment your brain operates like windows vista - ridiculously slow!\n")
+        self._current = cnt-2
+
+
     def game(self):
         overall_time = self._quiz()
         if not self._player in self._records.getScores():
             self._records.addPlayer(self._player)
+        if self._current == 0:
+            print(f"\n{self._player}, well your score is 0, so... That's it\n")
         if self._current > self._records.getScores()[self._player]:
             self._records.changeScore(self._player, self._current)
             self._records.exportScoreBoard()
-            print("You did have outdone yourself, mate! Frankly, you made it seem too easy...")
-            print(f"For this game you've gained {self._current} stunning points")
-            #print(f"Your average time per question is: {overall_time/self._current} seconds")
-            print(f"You have to be pround of yourself, {self._player}")
+            print("You did have outdone yourself, mate! Frankly, you made it seem too easy...\n")
+            print(f"For this game you've gained {self._current} stunning points\n")
+            print(f"Your average time per question is: {round(overall_time/self._current, 2)} seconds\n")
+            print(f"You have to be pround of yourself, {self._player}\n")
         else:
-            print("Well, it isn't that you are stupid, but rather our world is too complicated")
-            print(f"Number of points you struggled for: {self._current}")
-            #print(f"Your average time per question is:{overall_time/self._current}... Why in the world did it take you so long?!")
+            print("Well, it isn't that you are stupid, but rather our world is too complicated\n")
+            print(f"Number of points you struggled for: {self._current}\n")
+            print(f"Your average time per question is:{round(overall_time/self._current, 2)}... Why in the world did it take you so long?!\n")
             print(f"Don't worry, maybe next time you will study for the test, {self._player}")
 
 
